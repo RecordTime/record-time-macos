7c8593023637ea4b2e2435a25adb0331a7eba180
ltaoo <litaowork@aliyun.com>
2018-07-14 16:12:35 +0800

    feat: 以 cell view 展示数据

--- recordTime/ViewController/BlockedSettingsViewController.swift
+++ recordTime/ViewController/BlockedSettingsViewController.swift
@@ -19,15 +19,18 @@ class BlockedSettingsViewController: NSViewController {
         print("did load")
         self.updateData()
     }
+    override func viewWillAppear() {
+        print("will appeare")
+    }
     
     func updateData() {
 //        try! print(FileManager().contentsOfDirectory(atPath: "/Applications"))
 //        print(NSWorkspace.shared.runningApplications)
-        for app in NSWorkspace.shared.runningApplications {
-            print(app.icon)
-        }
+//        for app in NSWorkspace.shared.runningApplications {
+//            self.datas.append(["blocked": 0, "application": app.localizedName, "icon": app.icon])
+//        }
         self.datas = [
-            ["blocked": false, "application": "企业微信"]
+            ["blocked": "hello", "application": "企业微信"]
         ]
     }
     
@@ -44,5 +47,34 @@ extension BlockedSettingsViewController: NSTableViewDataSource {
         // 单元格数据
         let value = data[key!]
         return value
+//        let view = tableView.makeView(withIdentifier: key!, owner: self)
+//        let subviews = view?.subviews
+//        if (subviews?.count)! <= 0 {
+//            return nil
+//        }
+//        print(key?.rawValue, value, subviews)
+//        if key?.rawValue == "application" {
+//            let textField = subviews?[1] as! NSTextField
+//            if value != nil {
+//                textField.stringValue = value as! String
+//            }
+//        }
+//        if key?.rawValue == "blocked" {
+//            let textField = subviews?[1] as! NSTextField
+//            if value != nil {
+//                textField.stringValue = value as! String
+//            }
+//        }
+//        if key?.rawValue == "icon" {
+//            let imgView = subviews?[0] as! NSImageView
+//            if value != nil {
+//                imgView.image = value as! NSImage
+//            }
+//        }
+//        if key?.rawValue == "blocked" {
+//            let checkBoxField = subviews?[0] as! NSButton
+//            checkBoxField.state = NSControl.StateValue(rawValue: value as! Int)
+//        }
+        return view
     }
 }
